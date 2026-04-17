d9147d94a5a41760f9c547c752a7f6fcf21ea74c
s4nzzss-coder <s4nzzss@gmail.com.com>
2026-04-16 19:00:52 -0700

    cambio de codigos

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,12 @@
-# -*- coding: utf-8 -*-
-
-
 from django.db.models import Q
-from django.shortcuts import render, get_object_or_404, render
+from django.shortcuts import render, get_object_or_404, redirect
 
 
 from .models import Post, Category
+from .forms import CommentForm
 
 
-# Create your views here.
+# The code was ordened for a minor complex code.
 def home(request):
     posts = Post.objects.filter(status=Post.ACTIVE).order_by('-created_at')
     context = {
@@ -19,10 +17,29 @@ def home(request):
 
 def detail(request, id):
     post = get_object_or_404(Post, id=id, status=Post.ACTIVE)
+
+
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+
+
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+
+
+            return redirect('post_detail', id=id)
+    else:
+        form = CommentForm()
+
+
     context = {
         'post': post,
+        'form': form
     }
     return render(request, 'blog/detail.html', context)
+
 
 
 
